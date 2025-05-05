81919832db5d1cad0f40540c2a6ff66fbd7d2cc2
Augusto <augustocesarrezende64@gmail.com>
2025-05-05 19:58:41 -0300

    especificacao autoria testes

--- atividade-a3/src/test/java/com/augusto/atividadea3/FuncionarioTest.java
+++ atividade-a3/src/test/java/com/augusto/atividadea3/FuncionarioTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FuncionarioTest {
     private static final double SALARIO_MINIMO = 1518.00;
 
+    // @author Ruan Neres
     @Test
     public void testarConstrutorEntradasValida() {
         Funcionario funcionario = new Funcionario("João", 30, 60.72);
@@ -14,6 +15,7 @@ public class FuncionarioTest {
         assertEquals(60.72, funcionario.getValorHora(), 0.01);
     }
 
+    // @author Augusto Cesar
     @Test
     public void testarConstrutorEntradaHorasInvalida() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
@@ -27,6 +29,7 @@ public class FuncionarioTest {
         assertEquals("O número de horas trabalhadas deve estar entre 20 e 40.", exception.getMessage());
     }
 
+    // @author André Luiz Vicente
     @Test
     public void testarConstrutorEntradaValorHoraInvalida() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
@@ -40,6 +43,7 @@ public class FuncionarioTest {
         assertEquals("O valor por hora deve estar entre 4% e 10% do salário mínimo.", exception.getMessage());
     }
 
+    // @author Victor Hugo
     @Test
     public void testarConstrutorPagamentoInvalido() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
@@ -53,6 +57,7 @@ public class FuncionarioTest {
         assertTrue(exception.getMessage().contains("ultrapassar R$ 100.000.00"));
     }
 
+    // @author Artur Machado Soares
     @Test
     public void testarModificarHorasEntradaValida() {
         Funcionario funcionario = new Funcionario("Marta", 30, 60.72);
@@ -60,6 +65,7 @@ public class FuncionarioTest {
         assertEquals(25, funcionario.getHorasTrabalhadas());
     }
 
+    // @author Ruan Neres
     @Test
     public void testarModificarHorasEntradaInvalida() {
         Funcionario funcionario = new Funcionario("Rafael", 30, 60.72);
@@ -75,6 +81,7 @@ public class FuncionarioTest {
         assertEquals("O número de horas trabalhadas deve estar entre 20 e 40.", exception.getMessage());
     }
 
+    // @author Augusto Cesar
     @Test
     public void testarModificarHorasPagamentoInvalido() {
         Funcionario funcionario = new Funcionario("Paulo", 30, 60.72);
@@ -90,6 +97,7 @@ public class FuncionarioTest {
         assertTrue(exception.getMessage().contains("menor que o salário mínimo"));
     }
 
+    // @author André Luiz Vicente
     @Test
     public void testarModificarValorEntradaValida() {
         Funcionario funcionario = new Funcionario("Tereza", 30, 60.72);
@@ -97,6 +105,7 @@ public class FuncionarioTest {
         assertEquals(70.0, funcionario.getValorHora(), 0.01);
     }
 
+    // @author Victor Hugo
     @Test
     public void testarModificarValorEntradaInvalida() {
         Funcionario funcionario = new Funcionario("Fernanda", 30, 60.72);
@@ -112,6 +121,7 @@ public class FuncionarioTest {
         assertEquals("O valor por hora deve estar entre 4% e 10% do salário mínimo.", exception.getMessage());
     }
 
+    // @author Artur Machado Soares
     @Test
     public void testarModificarValorPagamentoInvalido() {
         Funcionario funcionario = new Funcionario("Eduardo", 30, 60.72);
